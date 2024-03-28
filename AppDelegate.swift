745fab7824b549966671b7284d74acdba4baa136
Ryszard Schossler <ryszard.schossler@gmail.com>
2024-03-28 10:31:39 +0100

    init commit

---
+++ AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MovieTrending
+//
+//  Created by Ryszard Schossler on 28/03/2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        return true
+    }
+
+}
+
